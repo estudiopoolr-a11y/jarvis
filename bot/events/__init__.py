e9ee25f7a02869a452548c00aca84f73fc0a5cdf
Pool <estudiopoolr@gmail.com>
2026-09-22 22:31:36 -0500

    Fix: Add missing __init__.py files to fix bot startup

---
+++ bot/events/__init__.py
@@ -0,0 +1,2 @@
+"""bot/events - Paquete de eventos del bot."""
+from bot.events.message_handler import handle_message
---
+++ modules/nlp/router/__init__.py
@@ -0,0 +1,2 @@
+"""modules/nlp/router - Paquete de enrutamiento NLP."""
+from modules.nlp.router.main import procesar_intencion_natural
