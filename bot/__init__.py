c3c3e2acb531e895f095fb9d4bf2df895614a7a4
Pool <estudiopoolr@gmail.com>
2026-09-24 10:56:38 -0500

    fix: set explicit discord intents

--- bot/__init__.py
+++ bot/__init__.py
@@ -22,6 +22,9 @@ ALLOWED_ROLE_IDS = [1537704466407497738]
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.messages = True
+intents.guilds = True
+intents.dm_messages = True
 intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
